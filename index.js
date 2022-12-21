3789571a11387eff47823d2a09b65facb12183f4
poojaarora2912 <poojaarora2912@gmail.com>
2022-12-21 21:33:11 +0530

    update: price of product
    update: price of product

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const orderNow = document.querySelector(".order-now");
+const priceperpiece = document.querySelector(".price-per-piece");
 
 function myFunction(){
     console.log("triggered");
@@ -6,4 +7,37 @@ function myFunction(){
     // document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+    increasePrice(value);
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+    console.log(value);
+    decreasePrice(value);
+  
+  }
+ function increasePrice(price){
+    var total = price * 750;
+    console.log(total);
+    document.getElementById("price-per-piece").value = total;
+ }
+
+ function decreasePrice(price){
+    var total = price * 750;
+    console.log("total price is" + total);
+    var totalprice = total;
+    console.log(totalprice);
+    document.getElementById("price-per-piece").value = totalprice;
+ }
+
 orderNow.addEventListener("click", myFunction);
+
